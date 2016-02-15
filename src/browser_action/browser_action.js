0586425dbb1addd5fc5fbc4772eea78e5ef8fa51
Joe Diragi <joyod3@gmail.com>
2016-02-15 16:46:29 -0500

    updated firebase api

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,11 +1,21 @@
 Firebase.enableLogging(true);
-var f = new Firebase('https://chrome-sample.firebaseio-demo.com/');
+var f = new Firebase('https://porthole.firebaseio.com/Ports');
+
+f.authAnonymously(function(error, authData) {
+  if (error) {
+    console.log("Login failed" +error, error);
+  } else {
+    console.log("Authenticated successfully w payload:", authData);
+  }
+}, {
+  remember: "sessionOnly"
+});
 
 f.transaction(function(curr) {
   if (isNaN(parseFloat(curr)))
     return 1; // initialize to 1.
   else
-    return curr + 1; // increment.
+    return "Fuck police"; // increment.
 }, function() {
     // Once the transaction has completed, update the UI (and watch for updates).
     f.on('value', function(s) {
