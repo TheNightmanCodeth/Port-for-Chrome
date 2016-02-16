aa6d2d7d1ef171445efbc8588e45a9cca8d0f3b2
Joe Diragi <joyod3@gmail.com>
2016-02-16 13:14:43 -0500

    Initial code push

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,24 +1,55 @@
 Firebase.enableLogging(true);
-var f = new Firebase('https://porthole.firebaseio.com/Ports');
+var f = new Firebase('https://porthole.firebaseio.com/');
+var username = "";
 
-f.authAnonymously(function(error, authData) {
-  if (error) {
-    console.log("Login failed" +error, error);
-  } else {
-    console.log("Authenticated successfully w payload:", authData);
-  }
-}, {
-  remember: "sessionOnly"
-});
+function register() {
 
-f.transaction(function(curr) {
-  if (isNaN(parseFloat(curr)))
-    return 1; // initialize to 1.
-  else
-    return "Fuck police"; // increment.
-}, function() {
-    // Once the transaction has completed, update the UI (and watch for updates).
-    f.on('value', function(s) {
-      document.getElementById('contents').innerHTML = s.val();
-    });
+  //Get the input from user
+  var emailVal = document.getElementById("email_input").value;
+  var usernVal = document.getElementById("user_input").value;
+  var passwVal = document.getElementById("passw_input").value;
+
+  //Create user
+  f.createUser({
+    email    : emailVal,
+    password : passwVal
+  }, function(error, userData) {
+      if (error) {
+        console.log("Error creating user: " +error);
+      } else {
+        console.log("Successfully created user account with uid: " +userData.uid);
+        username = usernVal;
+        login(emailVal, passwVal, usernVal);
+      }
   });
+
+}
+
+function writeLink(usern, link) {
+  console.log("writing link to firebase");
+  var portRef = f.child("ports");
+  portRef.child(usern).update({
+    link: link
+  });
+}
+
+function login(email, pass, usern) {
+  console.log("test");
+  f.authWithPassword({
+    email    : email,
+    password : pass
+  }, function(error, authData) {
+      if (error) {
+        console.log("Login failed: " +error);
+      } else {
+        console.log("Logged in: " +authData);
+        writeLink(usern, "https://www.reddit.com/r/trees/");
+      }
+  }, {
+    remember: "sessionOnly"
+  });
+}
+
+document.getElementById("submit_button").addEventListener("click", function() {
+  register();
+});
